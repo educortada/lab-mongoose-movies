5ed901cb07e5c5b0f198355796922b5e3807ea68
Edu Cortada <cortadaedu@gmail.com>
2019-03-02 19:13:30 +0100

    Iteration 4: Adding New Celebrities

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,25 @@ router.get('/celebrities', async (req, res, next) => {
   }
 });
 
+// Form to create a celebrity (GET)
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new');
+});
+
+// Send the data from the form to create the celebrity and save to the database (POST)
+router.post('/celebrities', async (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  // Values from those keys come from the form
+  const newCelebrity = { name, occupation, catchPhrase };
+  try {
+    // Save newCelebrity to DB
+    await Celebrity.create(newCelebrity);
+    res.redirect('/celebrities');
+  } catch (error) {
+    res.render('celebritie/new');
+  }
+});
+
 // Detail
 router.get('/celebrities/:id', async (req, res, next) => {
   const { id } = req.params;
